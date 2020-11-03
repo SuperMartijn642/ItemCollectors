74fe4577d8663883681aff47a827bc46b4e1f109
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-11-03 19:38:52 +0100

    initial 1.16.4 port

--- src/main/java/com/supermartijn642/itemcollectors/screen/CollectorScreen.java
+++ src/main/java/com/supermartijn642/itemcollectors/screen/CollectorScreen.java
@@ -36,7 +36,7 @@ public abstract class CollectorScreen<T extends CollectorContainer> extends Cont
     public void render(MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks){
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
-        this.func_230459_a_(matrixStack, mouseX, mouseY);
+        this.renderHoveredTooltip(matrixStack, mouseX, mouseY);
 
         CollectorTile tile = this.container.getTileOrClose();
         if(tile != null)
@@ -73,7 +73,7 @@ public abstract class CollectorScreen<T extends CollectorContainer> extends Cont
     protected abstract void drawText(MatrixStack matrixStack, CollectorTile tile);
 
     public void drawCenteredString(MatrixStack matrixStack, ITextComponent text, float x, float y){
-        this.font.func_243248_b(matrixStack, text, this.guiLeft + x - this.font.func_238414_a_(text) / 2f, this.guiTop + y, 4210752);
+        this.font.func_243248_b(matrixStack, text, this.guiLeft + x - this.font.getStringPropertyWidth(text) / 2f, this.guiTop + y, 4210752);
     }
 
     public void drawString(MatrixStack matrixStack, ITextComponent text, float x, float y){
