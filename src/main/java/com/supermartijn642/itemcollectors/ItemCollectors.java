52cd28afcb5ecf1f107dc14c8b57c204dfd613c1
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-01-02 20:23:30 +0100

    update version

--- src/main/java/com/supermartijn642/itemcollectors/ItemCollectors.java
+++ src/main/java/com/supermartijn642/itemcollectors/ItemCollectors.java
@@ -23,7 +23,7 @@ public class ItemCollectors {
 
     public static final String MODID = "itemcollectors";
     public static final String NAME = "Item Collectors";
-    public static final String VERSION = "1.0.7";
+    public static final String VERSION = "1.0.8";
 
     @Mod.Instance
     public static ItemCollectors instance;
