6f9300e1108a1d2cacd01198222b6159635182d7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-06-09 20:17:53 +0200

    Add dependencies for Core lib and Config lib

--- src/main/java/com/supermartijn642/itemcollectors/ItemCollectors.java
+++ src/main/java/com/supermartijn642/itemcollectors/ItemCollectors.java
@@ -16,12 +16,13 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 /**
  * Created 7/15/2020 by SuperMartijn642
  */
-@Mod(modid = ItemCollectors.MODID, name = ItemCollectors.NAME, version = ItemCollectors.VERSION)
+@Mod(modid = ItemCollectors.MODID, name = ItemCollectors.NAME, version = ItemCollectors.VERSION, dependencies = ItemCollectors.DEPENDENCIES)
 public class ItemCollectors {
 
     public static final String MODID = "itemcollectors";
     public static final String NAME = "Item Collectors";
     public static final String VERSION = "1.0.11";
+    public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.0.7,);required-after:supermartijn642corelib@[1.0.5,1.1.0)";
 
     @Mod.Instance
     public static ItemCollectors instance;
