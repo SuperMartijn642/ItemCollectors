eb431ca9647e357a3253a0475433ad1f983c296e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-06-09 14:00:37 +0200

    Move everything to Core lib Add config options

--- src/main/java/com/supermartijn642/itemcollectors/ClientProxy.java
+++ src/main/java/com/supermartijn642/itemcollectors/ClientProxy.java
@@ -2,8 +2,6 @@ package com.supermartijn642.itemcollectors;
 
 import com.supermartijn642.itemcollectors.screen.AdvancedCollectorContainer;
 import com.supermartijn642.itemcollectors.screen.AdvancedCollectorScreen;
-import com.supermartijn642.itemcollectors.screen.BasicCollectorContainer;
-import com.supermartijn642.itemcollectors.screen.BasicCollectorScreen;
 import net.minecraft.client.gui.ScreenManager;
 
 /**
@@ -12,7 +10,6 @@ import net.minecraft.client.gui.ScreenManager;
 public class ClientProxy {
 
     public static void registerScreen(){
-        ScreenManager.registerFactory(ItemCollectors.basic_collector_container, (ScreenManager.IScreenFactory<BasicCollectorContainer,BasicCollectorScreen>)(container, inventory, title) -> new BasicCollectorScreen(container));
         ScreenManager.registerFactory(ItemCollectors.advanced_collector_container, (ScreenManager.IScreenFactory<AdvancedCollectorContainer,AdvancedCollectorScreen>)(container, inventory, title) -> new AdvancedCollectorScreen(container));
     }
 
