653f0d8c53d9ec6a03efb4013615dd62320d954a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-06-12 19:36:34 +0200

    Fix the highlight rendering

--- src/main/java/com/supermartijn642/itemcollectors/CollectorTileRenderer.java
+++ src/main/java/com/supermartijn642/itemcollectors/CollectorTileRenderer.java
@@ -5,6 +5,7 @@ import com.supermartijn642.core.render.RenderUtils;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.util.math.vector.Vector3d;
 
 /**
  * Created 6/9/2021 by SuperMartijn642
@@ -17,7 +18,13 @@ public class CollectorTileRenderer extends TileEntityRenderer<CollectorTile> {
 
     @Override
     public void render(CollectorTile tile, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay){
-        if(tile.showArea)
+        if(tile.showArea){
+            matrixStack.push();
+            Vector3d camera = RenderUtils.getCameraPosition();
+            matrixStack.translate(camera.x, camera.y, camera.z);
+            matrixStack.translate(-tile.getPos().getX(), -tile.getPos().getY(), -tile.getPos().getZ());
             RenderUtils.renderBox(matrixStack, tile.getAffectedArea(), 245 / 255f, 212 / 255f, 66 / 255f);
+            matrixStack.pop();
+        }
     }
 }
