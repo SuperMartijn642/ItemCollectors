f28e4762cc65c33498fa6cf0957814c866dd6fb4
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-11-13 18:38:38 +0100

    Improve the collector area highlighting

--- src/main/java/com/supermartijn642/itemcollectors/CollectorTileRenderer.java
+++ src/main/java/com/supermartijn642/itemcollectors/CollectorTileRenderer.java
@@ -5,7 +5,9 @@ import com.supermartijn642.core.render.RenderUtils;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.math.AxisAlignedBB;
+
+import java.util.Random;
 
 /**
  * Created 6/9/2021 by SuperMartijn642
@@ -20,10 +22,19 @@ public class CollectorTileRenderer extends TileEntityRenderer<CollectorTile> {
     public void render(CollectorTile tile, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay){
         if(tile.showArea){
             matrixStack.push();
-            Vec3d camera = RenderUtils.getCameraPosition();
-            matrixStack.translate(camera.x, camera.y, camera.z);
             matrixStack.translate(-tile.getPos().getX(), -tile.getPos().getY(), -tile.getPos().getZ());
-            RenderUtils.renderBox(matrixStack, tile.getAffectedArea(), 245 / 255f, 212 / 255f, 66 / 255f);
+
+            AxisAlignedBB area = tile.getAffectedArea().grow(0.05f);
+
+            Random random = new Random(tile.getPos().hashCode());
+            float red = random.nextFloat();
+            float green = random.nextFloat();
+            float blue = random.nextFloat();
+            float alpha = 0.3f;
+
+            RenderUtils.renderBox(matrixStack, area, red, green, blue);
+            RenderUtils.renderBoxSides(matrixStack, area, red, green, blue, alpha);
+
             matrixStack.pop();
         }
     }
