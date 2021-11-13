26741a8ba971c4611386708aa99fae1396af2b19
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-11-13 16:07:26 +0100

    Improve the collector area highlighting

--- src/main/java/com/supermartijn642/itemcollectors/ClientProxy.java
+++ src/main/java/com/supermartijn642/itemcollectors/ClientProxy.java
@@ -4,11 +4,14 @@ import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.render.RenderUtils;
 import com.supermartijn642.itemcollectors.screen.BasicCollectorScreen;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -18,6 +21,8 @@ import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
+
+import java.util.Random;
 
 /**
  * Created 7/15/2020 by SuperMartijn642
@@ -46,8 +51,24 @@ public class ClientProxy {
 
             World world = ClientUtils.getWorld();
             TileEntity tile = world.getTileEntity(e.getTarget().getBlockPos());
-            if(tile instanceof CollectorTile)
-                RenderUtils.renderBox(((CollectorTile)tile).getAffectedArea(), 245 / 255f, 212 / 255f, 66 / 255f);
+            if(tile instanceof CollectorTile){
+                GlStateManager.pushMatrix();
+                Vec3d camera = RenderUtils.getCameraPosition();
+                GlStateManager.translate(-camera.x, -camera.y, -camera.z);
+
+                AxisAlignedBB area = ((CollectorTile)tile).getAffectedArea().grow(0.05f);
+
+                Random random = new Random(tile.getPos().hashCode());
+                float red = random.nextFloat();
+                float green = random.nextFloat();
+                float blue = random.nextFloat();
+                float alpha = 0.3f;
+
+                RenderUtils.renderBox(area, red, green, blue);
+                RenderUtils.renderBoxSides(area, red, green, blue, alpha);
+
+                GlStateManager.popMatrix();
+            }
         }
     }
 
--- src/main/java/com/supermartijn642/itemcollectors/CollectorTileRenderer.java
+++ src/main/java/com/supermartijn642/itemcollectors/CollectorTileRenderer.java
@@ -1,7 +1,12 @@
 package com.supermartijn642.itemcollectors;
 
 import com.supermartijn642.core.render.RenderUtils;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.Vec3d;
+
+import java.util.Random;
 
 /**
  * Created 6/9/2021 by SuperMartijn642
@@ -10,7 +15,23 @@ public class CollectorTileRenderer extends TileEntitySpecialRenderer<CollectorTi
 
     @Override
     public void render(CollectorTile tile, double x, double y, double z, float partialTicks, int destroyStage, float alpha){
-        if(tile.showArea)
-            RenderUtils.renderBox(tile.getAffectedArea(), 245 / 255f, 212 / 255f, 66 / 255f);
+        if(tile.showArea){
+            GlStateManager.pushMatrix();
+            Vec3d camera = RenderUtils.getCameraPosition();
+            GlStateManager.translate(-camera.x, -camera.y, -camera.z);
+
+            AxisAlignedBB area = tile.getAffectedArea().grow(0.05f);
+
+            Random random = new Random(tile.getPos().hashCode());
+            float red = random.nextFloat();
+            float green = random.nextFloat();
+            float blue = random.nextFloat();
+            alpha *= 0.3f;
+
+            RenderUtils.renderBox(area, red, green, blue);
+            RenderUtils.renderBoxSides(area, red, green, blue, alpha);
+
+            GlStateManager.popMatrix();
+        }
     }
 }
