c5340ce749edc9eaa57a93056a03487bc416c079
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-11-13 16:28:29 +0100

    Improve collector area highlighting

--- src/main/java/com/supermartijn642/itemcollectors/ClientProxy.java
+++ src/main/java/com/supermartijn642/itemcollectors/ClientProxy.java
@@ -1,5 +1,6 @@
 package com.supermartijn642.itemcollectors;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.supermartijn642.core.ClientUtils;
 import com.supermartijn642.core.render.RenderUtils;
 import com.supermartijn642.itemcollectors.screen.AdvancedCollectorContainer;
@@ -7,7 +8,9 @@ import com.supermartijn642.itemcollectors.screen.AdvancedCollectorScreen;
 import com.supermartijn642.itemcollectors.screen.BasicCollectorScreen;
 import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
@@ -15,6 +18,8 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import java.util.Random;
 
 /**
  * Created 7/15/2020 by SuperMartijn642
@@ -35,8 +40,24 @@ public class ClientProxy {
         public static void onBlockHighlight(DrawBlockHighlightEvent.HighlightBlock e){
             World world = ClientUtils.getWorld();
             TileEntity tile = world.getTileEntity(e.getTarget().getPos());
-            if(tile instanceof CollectorTile)
-                RenderUtils.renderBox(((CollectorTile)tile).getAffectedArea(), 245 / 255f, 212 / 255f, 66 / 255f);
+            if(tile instanceof CollectorTile){
+                GlStateManager.pushMatrix();
+                Vec3d camera = RenderUtils.getCameraPosition();
+                GlStateManager.translated(-camera.x, -camera.y, -camera.z);
+
+                AxisAlignedBB area = ((CollectorTile)tile).getAffectedArea().grow(0.05f);
+
+                Random random = new Random(tile.getPos().hashCode());
+                float red = random.nextFloat();
+                float green = random.nextFloat();
+                float blue = random.nextFloat();
+                float alpha = 0.3f;
+
+                RenderUtils.renderBox(area, red, green, blue);
+                RenderUtils.renderBoxSides(area, red, green, blue, alpha);
+
+                GlStateManager.popMatrix();
+            }
         }
     }
 
