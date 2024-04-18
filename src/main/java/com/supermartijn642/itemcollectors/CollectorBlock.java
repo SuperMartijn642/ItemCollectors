11b83677d2ece06b6f6a1d68cabb642da42c167a
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-04-18 22:48:02 +0200

    Always return SUCCESS when interacting with `CollectorBlock` (#34)

--- src/main/java/com/supermartijn642/itemcollectors/CollectorBlock.java
+++ src/main/java/com/supermartijn642/itemcollectors/CollectorBlock.java
@@ -79,7 +79,7 @@ public class CollectorBlock extends BaseBlock implements EntityHoldingBlock {
             ItemCollectorsClient.openBasicCollectorScreen(level, pos);
         else if(!level.isClientSide && this.hasFilter.get())
             CommonUtils.openContainer(new AdvancedCollectorContainer(ItemCollectors.filter_collector_container, player, level, pos));
-        return super.interact(state, level, pos, player, hand, hitSide, hitLocation);
+        return InteractionFeedback.SUCCESS;
     }
 
     @Override
