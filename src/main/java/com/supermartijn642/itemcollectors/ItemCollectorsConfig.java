8b0ae9e02c98878383e87dbe9246af7e617e2b5d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-07-23 19:16:37 +0200

    Add max insertions per tick config option #41

--- src/main/java/com/supermartijn642/itemcollectors/ItemCollectorsConfig.java
+++ src/main/java/com/supermartijn642/itemcollectors/ItemCollectorsConfig.java
@@ -15,6 +15,8 @@ public class ItemCollectorsConfig {
     public static final Supplier<Integer> advancedCollectorMaxRange;
     public static final Supplier<Boolean> advancedCollectorFilter;
 
+    public static final Supplier<Integer> maxInsertions;
+
     static{
         IConfigBuilder builder = ConfigBuilders.newTomlConfig("itemcollectors", null, false);
 
@@ -23,6 +25,10 @@ public class ItemCollectorsConfig {
         basicCollectorFilter = builder.comment("Should the basic item collector have a filter?").define("basicCollectorFilter", false);
         advancedCollectorMaxRange = builder.comment("What should be the maximum range of the advanced item collector?").define("advancedCollectorMaxRange", 7, 1, 10);
         advancedCollectorFilter = builder.comment("Should the advanced item collector have a filter?").define("advancedCollectorFilter", true);
+        builder.pop();
+
+        builder.push("Performance");
+        maxInsertions = builder.comment("What should be the maximum number of items an item collector should try to pick up and insert in a tick? Reducing this number may improve performance when there's a lot of items laying around, for example when storage is full. A value of -1 indicates no limit on the number of insertions.").define("maxInsertions", 20, -1, 10000);
         builder.pop();
 
         builder.build();
