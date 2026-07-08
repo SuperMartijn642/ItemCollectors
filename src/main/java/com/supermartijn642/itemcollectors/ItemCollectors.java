be9dfad1362845cd7c7e01180d17f74b3f4a867c
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-08 14:52:20 +0200

    Use `ClientUtils#getLevelNotAsClientLevel` to avoid crash on server #48

--- src/main/java/com/supermartijn642/itemcollectors/ItemCollectors.java
+++ src/main/java/com/supermartijn642/itemcollectors/ItemCollectors.java
@@ -67,7 +67,7 @@ public class ItemCollectors implements ModInitializer {
         // Container type
         handler.registerMenuType("filter_collector_container", () -> BaseContainerType.create(
             (container, data) -> data.writeBlockPos(container.getCollectorPosition()),
-            (player, data) -> new AdvancedCollectorContainer(filter_collector_container, player, ClientUtils.getWorld(), data.readBlockPos())
+            (player, data) -> new AdvancedCollectorContainer(filter_collector_container, player, ClientUtils.getLevelNotAsClientLevel(), data.readBlockPos())
         ));
     }
 
